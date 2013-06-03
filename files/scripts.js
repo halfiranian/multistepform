3de4aa5361dd4e56d35f449fd78d41b37e12e38e
Halfiranian <jamessadri@gmail.com>
2013-06-03 22:58:00 +0100

    Fixed final share stage

--- files/scripts.js
+++ files/scripts.js
@@ -7,6 +7,8 @@ var secondForm = '\
     <input type="tel" placeholder="Phone" name="phone">\
     '; // secondForm sets the form that replaces email on first submit
 
+var shareButtons = "<a href='' class='btn btn-primary fbshare'>Share on Facebook</a> <a href='' class='btn btn-info twshare'>Share on Twitter</a>"
+
 $("#multi .btn").click(function(){ // this is what happens when you hit submit
 	var email = $("input#email").val();	// set the variable 'email' with the contents of input element with id email
 	var firstname = $("input[name=firstname]").val();	// like above but targeting input with name firstname
@@ -26,25 +28,36 @@ $("#multi .btn").click(function(){ // this is what happens when you hit submit
  	console.log(dataString); // this is just for testing, it writes to the console in the browser
 	$.ajax({
       type: "POST",
-      url: "http://action.greenpeace.org.uk/page/signup/multistep",
+      url: "http://action.greenpeace.org.uk/page/signup/multistepTEST", // REMOVE TEST TO WORK
       data: dataString,
       success: function() {
 		// irrelevant here because it looks like it fails because of cross-domain ajax rules. need to check if this works in IE etc
       }
      });
+	if (stage == "final"){
+		makeShare();
+	} else {
     replaceForm(email);
+	}
     return false;
 });
 
 	
 function replaceForm(email){
 	$('#email').replaceWith(secondForm);
-	$('input').hide();	
-	$('input').fadeIn(2000);
+	$('input').hide().fadeIn(500);
 	$('#multi').append('<input type="hidden" name="email" value="' + email + '" id="email">');
 	$('#result').text("whoopwhoop");
 }	
 	
+function makeShare(){	// this is so hacky i hope nobody ever sees it :)
+	$('legend').after(shareButtons);
+	$('.fbshare').hide().fadeIn(500);
+	$('.twshare').hide().fadeIn(500);
+	$('input').remove();
+	$('button').remove();	
+	$('fieldset span').remove();
+}
 
 
 }); //closing jQuery opener here
